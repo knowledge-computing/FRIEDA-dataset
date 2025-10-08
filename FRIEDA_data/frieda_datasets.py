0c38afe41b15fbbb1e13565591e02ca963acb646
Jiyoon Pyo <jiyoonp0228@gmail.com>
2025-10-08 16:22:52 -0500

    update

--- FRIEDA_data/frieda_datasets.py
+++ FRIEDA_data/frieda_datasets.py
@@ -12,15 +12,32 @@ class FRIEDA(Dataset):
 
 
         if not os.path.exists(image_dir):
-            print("FRIEDA data directory cannot be found!")
+            print("[INFO] FRIEDA data directory cannot be found!")
 
             if download:
-                print("Downloading FRIEDA images.")
+                print("[INFO] Downloading FRIEDA images.")
                 self.download_image()
             else:
-                raise RuntimeError("Please allow downloading the dataset by setting '--download' for specify the correct directory.")
+                raise RuntimeError("Please allow downloading the dataset by setting '--download' or specify the correct root directory.")
+            
+        if not os.path.exists(annotation_file):
+            print("[INFO] FRIEDA question file cannot be found")
+
+            if download:
+                print("[INFO] Downloading FRIEDA question file.")
+                self.download_annotation()
+
+            else:
+                raise RuntimeError("Please allow downloading the dataset by setting '--download' or specify the correct root directory.")
     
+
     def download_image(self):
+        import gdown
+
+        return 0
+    
+
+    def download_annotation(self):
         import gdown
 
         return 0
